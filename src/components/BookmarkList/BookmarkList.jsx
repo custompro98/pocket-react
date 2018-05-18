7b655bdb254fdb6908f2ce8bc8947080c2c07c49
custompro98 <mitchjoa@gmail.com>
2018-05-18 13:28:57 -0500

    Refactor BookmarkList to dumb component

--- src/components/BookmarkList/BookmarkList.jsx
+++ src/components/BookmarkList/BookmarkList.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { graphql, QueryRenderer } from 'react-relay';
 
 import environment from '../../Environment';
@@ -7,55 +7,45 @@ import BookmarkCard from './BookmarkCard/BookmarkCard';
 
 import './BookmarkList.css';
 
-class BookmarkList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      bookmarks: []
-    };
-  }
-
-  render() {
-    return (
-      <QueryRenderer
-        environment={environment}
-        variables={{}}
-        query={graphql`
-          query BookmarkListQuery {
-            bookmarks {
+const BookmarkList = () => {
+  return (
+    <QueryRenderer
+      environment={environment}
+      variables={{}}
+      query={graphql`
+        query BookmarkListQuery {
+          bookmarks {
+            id,
+            title,
+            url,
+            owner {
               id,
-              title,
-              url,
-              owner {
-                id,
-                first_name
-              }
-              tags {
-                id,
-                name
-              }
+              first_name
+            }
+            tags {
+              id,
+              name
             }
           }
-        `}
-        render={({ error, props }) => {
-          if (error) return <div>{console.log(error)}Error!</div>;
-          if (!props) return <div>Loading...</div>;
-          return (
-            <div className="BookmarkList__Wrapper">
-              <ul className="BookmarkList__List">
-                {props.bookmarks.map((bookmark) => (
-                  <li key={bookmark.id} className="BookmarkList__Item">
-                    <BookmarkCard bookmark={bookmark} />
-                  </li>
-                ))}
-              </ul>
-            </div>
-          );
-        }}
-      />
-    );
-  }
+        }
+      `}
+      render={({ error, props }) => {
+        if (error) return <div>{console.log(error)}Error!</div>;
+        if (!props) return <div>Loading...</div>;
+        return (
+          <div className="BookmarkList__Wrapper">
+            <ul className="BookmarkList__List">
+              {props.bookmarks.map((bookmark) => (
+                <li key={bookmark.id} className="BookmarkList__Item">
+                  <BookmarkCard bookmark={bookmark} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        );
+      }}
+    />
+  );
 };
 
 export default BookmarkList;
